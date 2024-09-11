6801a77a2db3c726538e4eee8d076cc05e9a9398
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-09-11 17:33:41 -0300

    chore: remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller.js";
-import { AppService } from "./app.service.js";
 
 import * as Clients from "@/clients/index.js";
 import * as Modules from "@/modules/index.js";
 
 @Module({
   imports: [...Object.values(Clients), ...Object.values(Modules)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
