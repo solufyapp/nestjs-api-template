85109b7bb875873f702386a10700d506727beb66
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-09-11 17:33:04 -0300

    feat: users example module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
 
 import * as Clients from "@/clients/index.js";
+import * as Modules from "@/modules/index.js";
 
 @Module({
-  imports: [...Object.values(Clients)],
+  imports: [...Object.values(Clients), ...Object.values(Modules)],
   controllers: [AppController],
   providers: [AppService],
 })
